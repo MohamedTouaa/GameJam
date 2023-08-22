3d46ad258fc127b303ff94502c14ca1bec0adfd8
MohamedTouaa <121357213+MohamedTouaa@users.noreply.github.com>
2023-08-22 18:01:37 +0100

    Lvl 3 + Doors

---
+++ GameJame Game/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject door;         // Reference to the door GameObject
+    public float interactionRange = 2f;  // The range at which the player can interact with the button
+    public Sprite pressedSprite;    // The sprite to use when the button is pressed
+    public Sprite releasedSprite;   // The sprite to use when the button is released
+
+    private SpriteRenderer spriteRenderer;
+    private bool isPressed = false;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        // Check if the player is within interaction range and presses the "E" key
+        if (IsPlayerInRange() && Input.GetKeyDown(KeyCode.E))
+        {
+            isPressed = !isPressed;
+            spriteRenderer.sprite = isPressed ? pressedSprite : releasedSprite;
+            door.GetComponent<DoorController>().ToggleDoor();  // Call the ToggleDoor function in the DoorController script
+        }
+    }
+
+    private bool IsPlayerInRange()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            // Get the player's position
+            Vector2 playerPosition = new Vector2(player.transform.position.x, player.transform.position.y);
+
+            // Get the button's position
+            Vector2 buttonPosition = new Vector2(transform.position.x, transform.position.y);
+
+            // Calculate the distance between the player and the button
+            float distance = Vector2.Distance(playerPosition, buttonPosition);
+
+            // Check if the distance is within the interaction range
+            return distance <= interactionRange;
+        }
+
+        return false;
+    }
+}
