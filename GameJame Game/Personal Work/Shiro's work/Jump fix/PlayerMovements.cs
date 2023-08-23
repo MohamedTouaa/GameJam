6a61983e943afec0c68e6c46fef68ad3fa8664e8
MohamedTouaa <121357213+MohamedTouaa@users.noreply.github.com>
2023-08-23 15:46:29 +0100

    Jump animation fix

---
+++ GameJame Game/Personal Work/Shiro's work/Jump fix/PlayerMovements.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    [SerializeField]
+    private float speed = 8f;
+    [SerializeField]
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    private bool isWallSliding;
+    [SerializeField]
+    private float wallSlidingSpeed = 2f;
+
+    Animator animator;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private ParticleSystem dust;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            animator.SetBool("Jump", true);
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        WallSlide();
+        Flip();
+
+        // Set walk animation parameter based on player's velocity, but only if grounded
+        if (IsGrounded())
+        {
+            animator.SetBool("isWalking", Mathf.Abs(rb.velocity.x) > 0.1f);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false); // Not grounded, don't play walk animation
+        }
+
+        // Reset jump animation parameter when player is grounded and moving downward
+        if (IsGrounded() && rb.velocity.y <= 0f)
+        {
+            animator.SetBool("Jump", false);
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+    }
+
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void WallSlide()
+    {
+        if (IsWalled() && !IsGrounded() && horizontal != 0f)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            activeDust();
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    private void activeDust()
+    {
+        dust.Play();
+    }
+
+
+}
